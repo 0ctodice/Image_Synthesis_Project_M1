9aeef916c6dc69c93961c5052a4722297cf14018
8ctodice <thomas2dumont@gmail.com>
2022-03-12 08:41:56 +0100

    upgraded scale factoring on table

--- main.c
+++ main.c
@@ -29,23 +29,19 @@ Shape *cube_can;
 
 Node *table;
 
-double scale = 0.05;
+double init_scale = 1.;
 
 Node *gl_pied(double x, double y, double z, Node *initNode)
 {
   Node *pied = init_node();
 
-  pied->scale_factor = initNode->scale_factor;
-
-  *(pied->Md) = *(initNode->Md);
-  *(pied->Md) = g3x_Mat_x_Mat(*(pied->Md), g3x_Homothetie3d(0.12, 0.12, 0.99));
   pied->col = (G3Xcolor){0.30, 0.20, 0.10, 1.};
-  pied->mat[0] = 0.5;
-  pied->mat[1] = 0.75;
-  pied->mat[2] = pied->mat[3] = 0.;
+  pied->mat[0] = 0.5, pied->mat[1] = 0.75, pied->mat[2] = pied->mat[3] = 0.;
   pied->instance = cylinder_can;
 
+  *(pied->Md) = g3x_Mat_x_Mat(*(initNode->Md), g3x_Homothetie3d(0.12, 0.12, 0.99));
   *(pied->Md) = g3x_Mat_x_Mat(*(pied->Md), g3x_Translation3d(x, y, z));
+  *(pied->scale_factor) = g3x_Mat_x_Vector(*(pied->Md), *(initNode->scale_factor));
 
   return pied;
 }
@@ -55,19 +51,18 @@ Node *gl_table()
   // Noeud Principal
   Node *table = init_node();
   *(table->Md) = g3x_Mat_x_Mat(*(table->Md), g3x_Homothetie3d(0.75, 0.75, 0.75));
+  *(table->scale_factor) = g3x_Mat_x_Vector(*(table->Md), (G3Xvector){init_scale, init_scale, init_scale});
 
   // Plateau
   Node *plateau = init_node();
-  *(plateau->Md) = *(table->Md);
-  *(plateau->Md) = g3x_Mat_x_Mat(*(plateau->Md), g3x_Translation3d(0., 0., 1.));
-  *(plateau->Md) = g3x_Mat_x_Mat(*(plateau->Md), g3x_Translation3d(0., 0., -0.1));
-  *(plateau->Md) = g3x_Mat_x_Mat(*(plateau->Md), g3x_Homothetie3d(1.8, 1., 0.1));
   plateau->col = (G3Xcolor){0.50, 0.40, 0.20, 1.};
-  plateau->mat[0] = 0.5;
-  plateau->mat[1] = 0.65;
-  plateau->mat[2] = plateau->mat[3] = 0.5;
+  plateau->mat[0] = 0.5, plateau->mat[1] = 0.65, plateau->mat[2] = plateau->mat[3] = 0.5;
   plateau->instance = cube_can;
   plateau->scale_factor = table->scale_factor;
+  *(plateau->Md) = *(table->Md);
+  *(plateau->Md) = g3x_Mat_x_Mat(*(plateau->Md), g3x_Homothetie3d(1.8, 1., 0.1));
+  *(plateau->Md) = g3x_Mat_x_Mat(*(plateau->Md), g3x_Translation3d(0., 0., 9.));
+  *(plateau->scale_factor) = g3x_Mat_x_Vector(*(plateau->Md), *(plateau->scale_factor));
 
   // Pieds
   Node *pied1 = gl_pied(+12.5, +5.5, -0.19, table);
@@ -98,17 +93,11 @@ static void init(void)
 /* la fonction de contrôle : appelée 1 seule fois, juste après <init> */
 static void ctrl(void)
 {
-  g3x_SetScrollWidth(11);
-
-  int id;
-  id = g3x_CreateScrollv_d("step", &scale, 0.01, 0.5, 1., "scale factor");
-  g3x_SetScrollColor(id, G3Xrb_c);
 }
 
 /* la fonction de dessin : appelée en boucle */
 static void draw(void)
 {
-  *(table->scale_factor) = (G3Xvector){scale, scale, scale};
   draw_node(table);
 }
 
